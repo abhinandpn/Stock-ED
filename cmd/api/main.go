c7905f5dfe3d5824fcdb663d9ecd34266ec23d0b
abhinandpn <abhinandpn4067@gmail.com>
2024-07-12 00:52:15 +0530

    main function setup complte

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/abhinandpn/Stock-ED/pkg/loader" // Adjust import path as needed
+)
 
 func main() {
-	fmt.Println("server started.....")
+	// Register handlers for CSS, HTML, JS
+	loader.AboutCssLoader()
+	loader.HtmlLoader()
+	loader.JSLoader()
+
+	// Serve static files (CSS, JS, HTML)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/stylesheets/", fs)
+	http.Handle("/scripts/", fs)
+
+	// Start HTTP server
+	http.ListenAndServe(":8080", nil)
 }

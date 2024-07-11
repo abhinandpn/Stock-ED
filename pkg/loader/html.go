38bb4f4b72bcad73eaef61b4e3782380f7c553fa
abhinandpn <abhinandpn4067@gmail.com>
2024-07-12 00:51:27 +0530

    html file loading package setup complete

--- pkg/loader/html.go
+++ pkg/loader/html.go
@@ -1,1 +1,19 @@
 package loader
+
+import (
+    "net/http"
+)
+
+func HtmlLoader() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/html/home.html")
+    })
+
+    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/html/about.html")
+    })
+
+    http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/html/contact.html")
+    })
+}

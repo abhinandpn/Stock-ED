264e5e080e3dfa1b187ca0c6e5f9d380be72efa9
abhinandpn <abhinandpn4067@gmail.com>
2024-07-12 00:50:19 +0530

    css file loading package setup complete

--- pkg/loader/css.go
+++ pkg/loader/css.go
@@ -1,1 +1,19 @@
 package loader
+
+import (
+    "net/http"
+)
+
+func AboutCssLoader() {
+    http.HandleFunc("/stylesheets/about.css", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/css/about.css")
+    })
+
+    http.HandleFunc("/stylesheets/contact.css", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/css/contact.css")
+    })
+
+    http.HandleFunc("/stylesheets/home.css", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/css/home.css")
+    })
+}

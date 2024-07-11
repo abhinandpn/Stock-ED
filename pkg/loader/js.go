e1324fd3506ef9ddbd8bd2f35e5524b956cdd42e
abhinandpn <abhinandpn4067@gmail.com>
2024-07-12 00:50:43 +0530

    js file loading package setup complete

--- pkg/loader/js.go
+++ pkg/loader/js.go
@@ -1,1 +1,19 @@
 package loader
+
+import (
+    "net/http"
+)
+
+func JSLoader() {
+    http.HandleFunc("/scripts/about.js", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/js/about.js")
+    })
+
+    http.HandleFunc("/scripts/contact.js", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/js/contact.js")
+    })
+
+    http.HandleFunc("/scripts/home.js", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./static/js/home.js")
+    })
+}

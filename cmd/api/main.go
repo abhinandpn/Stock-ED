344c1726a768dbd401f5078011e78b325273aeb5
abhinandpn <abhinandpn4067@gmail.com>
2024-07-13 18:54:46 +0530

    main file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/abhinandpn/Stock-ED/pkg/loader" // Adjust import path as needed
+    "net/http"
+    "github.com/abhinandpn/Stock-ED/pkg/loader" // Adjust import path as needed
 )
 
 func main() {
-	// Register asset loader
-	// loader.AssetLoader()
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./static/asset"))))
+    // Register asset loader
+    loader.AssetLoader()
 
-	// Register handlers for CSS, HTML, JS
-	loader.CSSLoader()
-	loader.HtmlLoader()
-	loader.JSLoader()
+    // Register handlers for CSS, HTML, JS
+    loader.CSSLoader()
+    loader.HtmlLoader()
+    loader.JSLoader()
 
-	// Serve static files (CSS, JS, HTML)
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/stylesheets/", fs)
-	http.Handle("/scripts/", fs)
+    // Serve static files (CSS, JS, HTML)
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/stylesheets/", fs)
+    http.Handle("/scripts/", fs)
 
-	// Start HTTP server
-	http.ListenAndServe(":8080", nil)
+    // Start HTTP server
+    http.ListenAndServe(":8080", nil)
 }
